9f509514682975fa9e488ead1cf917dc878627a4
Corey Abshire <corey.abshire@gmail.com>
2016-01-02 21:27:44 -0600

    Fixed distance function; refactored hull class.
    I learned that the Delaunay code and and plane distance function
    wasn't working the way I expected. I refactored the hull code a bit
    so that Hull is a new class with some of the logic. I also found a
    pointTriangleDistance function that does seem to work as I need and
    plugged it in. The classifications for points outside the hulls are
    much more believable now, but the code is noticeably slower. I think
    there are several optimization opportunities, but as it does seem to
    work quite a bit better now this seems to be a good checkpoint.

--- hull_classify.py
+++ hull_classify.py
@@ -6,6 +6,8 @@ from scipy.spatial import Delaunay
 from scipy.spatial import ConvexHull
 from colormath.color_objects import LabColor, sRGBColor
 from colormath.color_conversions import convert_color
+from pointTriangleDistance import pointTriangleDistance
+
 
 filename = '../data/old_data.csv'
 data = pd.read_csv(filename)
@@ -26,76 +28,70 @@ namemap = {
 
 c = names.apply(lambda x: namemap[x])
 
+class Hull(object):
+
+    def __init__(self, points):
+        self.hull = ConvexHull(points)
+        self.delaunay = Delaunay(points.irow(self.hull.vertices))
+        self.tris = [np.array([[x for x in self.hull.points[s[i]]] for i in range(3)]) for s in self.hull.simplices]
+
+    def centroid(self):
+        return self.delaunay.points.mean(0)
+
+    def distance(self, xi):
+        ds = [pointTriangleDistance(tri, xi)[0] for tri in self.tris]
+        #return min(np.abs(self.delaunay.plane_distance(xi)))
+        return min(ds)
+
+    def contains(self, X):
+        return self.delaunay.find_simplex(X)
+
 class ConvexHullFuzzyClassifier(object):
 
     def __init__(self, thresholds):
+        self.ks = []
         self.hulls = {}
-        self.hullsd = {}
         self.thresholds = thresholds
 
     def fit(self, X, y):
-        for yi in unique(y):
-            print 'fitting', yi
-            ccoords = coords[y==yi]
-            hull = ConvexHull(ccoords)
-            self.hulls[yi] = hull
-            hulld = Delaunay(ccoords.irow(hull.vertices))
-            self.hullsd[yi] = hulld
+        self.ks = unique(y)
+        self.hulls = {k:Hull(X[y==k]) for k in self.ks}
 
     def predict(self, X):
-        y = {}
-        np.ndarray((len(X),len(self.hullsd.keys())))
-        for yi in self.hullsd.keys():
-             y[yi] = self.hullsd[yi].find_simplex(X)
-        y = pd.DataFrame(y)
+        y = pd.DataFrame({k:self.hulls[k].contains(X) for k in self.ks})
 
         # The output of find_simplex above will return the index of the
         # simplex for the point given and -1 otherwise, but for our function
         # we need the fuzzy values. To get there, we first convert to a simple
         # 0 and 1 based on those results.
-        y = y.apply(lambda col: col.apply(lambda v: 0 if v < 0 else 1))
+        y = y.apply(lambda c: c.apply(lambda v: 0 if v < 0 else 1))
 
         # If a point falls within two hulls, we have to normalize so that
         # the value for all classes sum to one.
         colsum = sum(y, 1) # compute the current col sums
         inhull = colsum > 0
-        y[inhull] = y[inhull].apply(lambda col: col / colsum)
+        y[inhull] = y[inhull].apply(lambda c: c / colsum)
 
         # For points outside any hulls, we need a distance matrix.
-        dist = self.compute_distance_matrix(X)
-        thresh = self.apply_thresholds(dist)
+        dist = X.apply(self.distance_vector, 1)
+        thresh = dist.apply(self.vector_threshold, 1)
         for k in y:
             y.loc[~inhull,k] = thresh[~inhull][k]
-
-        # We then apply the threshold function so that only up to 
-        # four categories are considered.
-
-        # Then for each pair of distances we apply the sigmoid function
-        # to adjust the transition between hulls, and then apply weights.
 
         ynames = y.apply(lambda x: np.argmax(x), 1)
         
         return y, dist, thresh, ynames, inhull
 
-    def compute_distance_matrix(self, X):
-        return X.apply(self.find_point_distance_vector, 1)
-
-    def find_point_distance_vector(self, xi):
+    def distance_vector(self, xi):
         """Finds the minimum distance of each point in xi to each hull."""
-        d = {}
-        ks = self.hullsd.keys()
-        for yi in ks:
-            h = self.hullsd[yi]
-            d[yi] = min(np.abs(h.plane_distance(xi)))
-        return pd.Series([d[k] for k in ks], index=ks)
+        d = {k:self.hulls[k].distance(xi) for k in self.ks}
+        return pd.Series([d[k] for k in self.ks], index=self.ks)
 
-    def apply_thresholds(self, d):
-        return d.apply(self.find_vector_threshold, 1)
-
-    def find_vector_threshold(self, d):
-        ks = self.hullsd.keys()
-        ss = sorted(ks, key=lambda k: d[k])
-        sd = dict((k, 0.0) for k in ks)
+    def vector_threshold(self, d):
+        # We then apply the threshold function so that only up to 
+        # four categories are considered.
+        ss = sorted(self.ks, key=lambda k: d[k])
+        sd = {k:0.0 for k in self.ks}
         sd[ss[0]] = d[ss[0]]
         ssum = d[ss[0]]
         th = 0
@@ -113,6 +109,8 @@ class ConvexHullFuzzyClassifier(object):
                     th += 1
                 else:
                     break
+        # Then for each pair of distances we apply the sigmoid function
+        # to adjust the transition between hulls, and then apply weights.
         if len(inthresh) == 1:
             sd[inthresh[0]] = 1.0
         elif len(inthresh) == 2:
@@ -126,10 +124,10 @@ class ConvexHullFuzzyClassifier(object):
                 a = sd[inthresh[i]]
                 b = sd[inthresh[j]]
                 total = a + b
-        return pd.Series([sd[k] for k in ks], index=ks)
+        return pd.Series([sd[k] for k in self.ks], index=self.ks)
 
     def hull_centroid(self, k):
-        return self.hullsd[k].points.mean(0)
+        return self.hulls[k].centroid()
 
 def point_lab(p):
     return LabColor(p[0],p[1],p[2])
--- validate_delaunay_understanding.py
+++ validate_delaunay_understanding.py
@@ -1,14 +1,27 @@
+import numpy as np
+from scipy.spatial import Delaunay, ConvexHull
+from pointTriangleDistance import pointTriangleDistance
 
-
-ps = np.array([[ 0.0,  0.0,  1.0],
-               [ 0.0,  1.0, -1.0],
-               [ 0.0, -1.0, -1.0],
-               [-1.0,  0.0,  1.0]])
-d = Delaunay(ps)
 
 ps = np.array([[ 0.0, -1.0,  1.0],
                [ 0.0,  1.0,  1.0],
                [ 0.0,  1.0, -1.0],
                [ 0.0, -1.0, -1.0],
-               [-1.0,  0.0,  0.0]])
+               [-1.0, -1.0,  1.0],
+               [-1.0,  1.0,  1.0],
+               [-1.0,  1.0, -1.0],
+               [-1.0, -1.0, -1.0]])
 d = Delaunay(ps)
+h = ConvexHull(ps)
+
+print 'Delaunay'
+for s in d.simplices:
+    print s
+    
+print 'Convex hull'
+for s in h.simplices:
+    tri = np.array([[x for x in h.points[s[i]]] for i in range(3)])
+    print tri
+    p = np.array([2,0,0])
+    print p
+    print pointTriangleDistance(tri, p)
